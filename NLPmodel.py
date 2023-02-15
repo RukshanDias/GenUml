94083f3b7bd389e867af7e654e60ca0faf38e6dd
AsithaManupiya2000 <124169169+AsithaManupiya2000@users.noreply.github.com>
2023-02-15 23:53:37 +0530

    NLP Model update #2
    stopped adding repeating entries for actors and usecases.

--- NLPmodel.py
+++ NLPmodel.py
@@ -4,8 +4,8 @@ nlp = spacy.load("en_core_web_sm")
 
 def extract_subject_verb_object(paragraph):
     doc = nlp(paragraph)
-    actors = []
-    usecases = []
+    actors = set()
+    usecases = set()
     for sent in doc.sents:
         subjects = []
         verbs = []
@@ -19,12 +19,11 @@ def extract_subject_verb_object(paragraph):
                 for obj in token.rights:
                     if obj.dep_ == "dobj":
                         objects.append(obj.text)
-        actors.extend(subjects)
-        usecases.append(" ".join(verbs + objects))
-    return actors, usecases
+        actors.update(subjects)
+        usecases.add(" ".join(verbs + objects))
+    return list(actors), list(usecases)
 
 paragraph = input("Enter a paragraph: ")
 actors, usecases = extract_subject_verb_object(paragraph)
 print("Actors:", actors)
 print("Usecases:", usecases)
-   
