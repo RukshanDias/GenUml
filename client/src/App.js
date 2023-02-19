5ae077be591facf21b1182f63c1348d26690ff13
Rukshan Dias <108785874+RukshanDias@users.noreply.github.com>
2023-02-19 22:57:30 +0530

    created basic edit page.
    basic edit page. has linked with other pages.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home/Home.js';
 import Signup from './pages/Signup/Signup';
 import UserInput from './pages/UserInput/UserInput';
 import Download from './pages/Download/Download';
+import Edit from './pages/Edit/Edit';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/sign-up" element={<Signup />} />
           <Route path="/userRequirment" element={<UserInput />} />
           <Route path="/download" element={<Download />} />
+          <Route path="/edit" element={<Edit />} />
         </Routes>
       </Router>
     </div>
