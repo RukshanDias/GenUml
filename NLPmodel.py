7a89501f7597e0f3b3846e791aa25c6dbdc9ed89
AsithaManupiya2000 <124169169+AsithaManupiya2000@users.noreply.github.com>
2023-03-13 10:47:36 +0530

    A new model for NLP using word chunks

--- NLPmodel.py
+++ NLPmodel.py
@@ -1,32 +1,44 @@
 import spacy
 
+# Load the language model
 nlp = spacy.load("en_core_web_sm")
 
-def extract_subject_verb_object(paragraph):
-    doc = nlp(paragraph)
-    actors = set()
-    usecases = set()
-    for sent in doc.sents:
-        subjects = []
-        verbs = []
-        objects = []
-        for token in sent:
-            if token.dep_ == "ROOT" and token.pos_ == "VERB":
-                verbs.append(token.lemma_)
-                for subject in token.lefts:
-                    if subject.dep_ == "nsubj":
-                        if subject.pos_ != "PRON":
-                            subjects.append(subject.text.lower())
-                for obj in token.rights:
-                    if obj.dep_ == "dobj":
-                        objects.append(obj.text.lower())
-        actors.update(subjects)
-        usecases.add(" ".join(verbs + objects))
-    return list(map(str, actors)), list(map(str, usecases))
+# Define the text to process
+text = "“A customer calls a hair salon to make an appointment for a haircut. The receptionist checks the availability of the hairdresser and schedules the appointment for the next available time slot."
 
-paragraph = input("Enter a paragraph: ")
-doc = nlp(paragraph)
-new_paragraph = " ".join([token.text for token in doc if not token.pos_ == "PRON"])
-actors, usecases = extract_subject_verb_object(new_paragraph)
+# Process the text with the language model
+doc = nlp(text)
+
+# Define a function to check if a token is a verb
+def is_verb(token):
+    return token.pos_ == "VERB"
+
+# Define a function to check if a token is an object
+def is_object(token):
+    return token.dep_ == "dobj"
+
+# Define a function to check if a token is a subject
+def is_subject(token):
+    return token.dep_ == "nsubj" and token.pos_ != "PRON"
+
+# Initialize empty sets to store unique actors and usecases
+actors = set()
+usecases = set()
+
+# Loop through the tokens in the processed text
+for token in doc:
+    # If the token is a verb, and it has an object
+    if is_verb(token) and any(is_object(child) for child in token.children):
+        # Get the verb and its object
+        verb = token.lemma_
+        obj = [child for child in token.children if is_object(child)][0]
+        # Add the chunk to the set of usecases
+        usecases.add(f"{verb} {obj}")
+    # If the token is a subject
+    elif is_subject(token):
+        # Add the root form of the subject to the set of actors
+        actors.add(token.lemma_.lower())
+
+# Print the final sets of chunks
 print("Actors:", actors)
-print("Usecases:", usecases)
+print("Use Cases:", usecases)
