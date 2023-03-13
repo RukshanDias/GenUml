dfd48e31a8ca869ebb046e375bb81a4fdeb852ce
Heshanwicky <heshanwick2@gmail.com>
2023-03-13 10:11:37 +0530

    final edit

--- serverside/spelling_grammer_checker/grammar_checker.py
+++ serverside/spelling_grammer_checker/grammar_checker.py
@@ -4,29 +4,20 @@ from gingerit.gingerit import GingerIt
 
 class GrammarChecker:
     parser = GingerIt()
-    last_para_list = []
+
     @classmethod
-    def check_grammar_single_sentence(cls,text:str):
+    def __check_grammar_single_sentence(cls,text:str) -> str:
         results = cls.parser.parse(text)
-        correction_list = results['corrections']
-        if len(correction_list) != 0:
-            if '\n' in results['text']:
-                results['text'] = results['text'].replace("\n", "")
-            print(f"Incorrect sentence: {results['text']}\n")
-            for i in range(len(correction_list)):
-                print(f"Incorrect word: {correction_list[i]['text']}\nCorrected_Word: {correction_list[i]['correct']}\n")
-            print(f"Corrected sentence: {results['result']}\n___________________________________________________________________________________________________________________________________________________________________________________")
-            cls.last_para_list.append(results['result'])
+        return results['result']
+
+
 
     @classmethod
-    def check_grammar_in_files(cls,file_name:str):
-        with open(file_name,'r') as file:
-            sentences_list=file.read().split('.')
-            for sentence in sentences_list:
-                cls.check_grammar_single_sentence(text=sentence)
-
-    @classmethod
-    def check_grammar_in_paragraph(cls,para:str):
+    def check_grammar_in_paragraph(cls,para:str) -> str:
+        final_para_list = []
         sentence_list = para.split('.')
         for sentence in sentence_list:
-            cls.check_grammar_single_sentence(text=sentence)
+            corrected_sen = cls.__check_grammar_single_sentence(text=sentence)
+            final_para_list.append(corrected_sen)
+        final_para = ".".join(final_para_list)
+        return final_para
