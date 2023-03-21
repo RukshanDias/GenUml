4f5d9ab932efc83519f3da6e3d49600930dc3c23
AsithaManupiya2000 <124169169+AsithaManupiya2000@users.noreply.github.com>
2023-03-21 11:43:42 +0530

    Update NLPmodel.py

--- serverside/NLP_Model/NLPmodel.py
+++ serverside/NLP_Model/NLPmodel.py
@@ -3,10 +3,7 @@ import spacy
 # Load the language model
 nlp = spacy.load("en_core_web_sm")
 
-# Define the text to process
-text = "A customer logs into an e-commerce website to purchase a product. The software system checks the availability of the product in stock, processes the payment, and provides the customer with a confirmation of their order."
-# Process the text with the language model
-doc = nlp(text)
+
 
 # Define a function to check if a token is a verb
 def is_verb(token):
@@ -20,37 +17,44 @@ def is_object(token):
 def is_subject(token):
     return token.dep_ == "nsubj" and token.pos_ != "PRON"
 
-# Initialize empty sets to store unique actors and usecases
-actors = set()
-usecases = set()
+def extractActorsUc(text):
+    # Process the text with the language model
+    doc = nlp(text)
+    # Initialize empty sets to store unique actors and usecases
+    actors = set()
+    usecases = set()
 
-# initialize a variable and a dictonary
-activeActor = "";
-actorUsecaseDictionary = {}
+    # initialize a variable and a dictonary
+    activeActor = ""
+    actorUsecaseDictionary = {}
 
-# Loop through the tokens in the processed text
-for token in doc:
-    # If the token is a verb, and it has an object
-    if is_verb(token) and any(is_object(child) for child in token.children):
-        # Get the verb and its object
-        verb = token.lemma_
-        obj = [child for child in token.children if is_object(child)][0]
-        # Add the chunk to the set of usecases
-        usecases.add(f"{verb} {obj}")
+    # Loop through the tokens in the processed text
+    for token in doc:
+        # If the token is a verb, and it has an object
+        if is_verb(token) and any(is_object(child) for child in token.children):
+            # Get the verb and its object
+            verb = token.lemma_
+            obj = [child for child in token.children if is_object(child)][0]
+            # Add the chunk to the set of usecases
+            usecases.add(f"{verb} {obj}")
 
-        # dictionary part
-        if activeActor not in actorUsecaseDictionary:
-          actorUsecaseDictionary[activeActor] = [(f"{verb} {obj}")]
-        else:
-          actorUsecaseDictionary[activeActor].append(f"{verb} {obj}")
+            # dictionary part
+            if activeActor not in actorUsecaseDictionary:
+              actorUsecaseDictionary[activeActor] = [(f"{verb} {obj}")]
+            else:
+              actorUsecaseDictionary[activeActor].append(f"{verb} {obj}")
 
-    # If the token is a subject
-    elif is_subject(token):
-        # Add the root form of the subject to the set of actors
-        actors.add(token.lemma_.lower())
-        activeActor=token.lemma_.lower() #change active actor
+        # If the token is a subject
+        elif is_subject(token):
+            # Add the root form of the subject to the set of actors
+            actors.add(token.lemma_.lower())
+            activeActor=token.lemma_.lower() #change active actor
+            
+    return actorUsecaseDictionary
+        
+
 
 # Print the final sets of chunks
-print("Actors:", actors)
-print("Use Cases:", usecases)
-print("dictionary",actorUsecaseDictionary)
+# print("Actors:", actors)
+# print("Use Cases:", usecases)
+# print("dictionary",actorUsecaseDictionary)
